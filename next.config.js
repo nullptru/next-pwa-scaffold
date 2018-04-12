bbe29ea0338c118e0545264013115ad813ddc942
nullptru <nullptru@gmail.com>
2018-04-12 16:33:27 +0800

    config build id

--- next.config.js
+++ next.config.js
@@ -2,18 +2,20 @@ const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const withLess = require('@zeit/next-less');
 const path = require('path');
 
+const buildId = 'your-build-id';
 module.exports = withLess({
   cssModules: true,
   distDir: 'build',
   generateEtags: false,
   generateBuildId: async () => {
-    return 'geass-blog';
+    return buildId;
   },
-  webpack: (config) => {
+  webpack: (oldConfig) => {
     // Fixes npm packages that depend on `fs` module
+    const config = { ...oldConfig };
     config.node = {
-      fs: 'empty'
-    }
+      fs: 'empty',
+    };
 
     config.plugins.push(
       new SWPrecacheWebpackPlugin({
@@ -22,27 +24,27 @@ module.exports = withLess({
         navigateFallback: '/',
         filename: 'service-worker.js',
         staticFileGlobs: [
-          "static/**/*.*",
+          'static/**/*.*',
         ],
         mergeStaticsConfig: true,
         staticFileGlobsIgnorePatterns: [/^build\/dist\/.*/],
         stripPrefixMulti: {
           [path.resolve(__dirname, 'build/static')]: '_next/static',
-          [path.resolve(__dirname, 'build/bundles/pages')]: '_next/geass-blog/page',
-          [path.resolve(__dirname, 'build')]: '_next/geass-blog',
+          [path.resolve(__dirname, 'build/bundles/pages')]: `_next/${buildId}/page`,
+          [path.resolve(__dirname, 'build')]: `_next/${buildId}`,
         },
         runtimeCaching: [
           {
             handler: 'networkFirst',
-            urlPattern: /^https?.*^[\.js,\.css,\.jpg,\.png]$/
+            urlPattern: /^https?.*^[.(js|css|png|jpg)]$/,
           }, {
             handler: 'cacheFirst',
-            urlPattern: /\/$/
-          }
-        ]
+            urlPattern: /\/$/,
+          },
+        ],
       })
-    )
+    );
 
-    return config
-  }
+    return config;
+  },
 });
